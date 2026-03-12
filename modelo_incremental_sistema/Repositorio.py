c9f2ccc61ba3e89a470de1fcbe89658c0c43c354
ProencaPedro <pedro.henrikproenca@gmail.com>
2026-03-12 15:33:17 -0300

    Adicionando estrutura inicial do Sistema Escolar - Aula de Modelagem de Sistemas
    Sistema de Gestão Escolar. Projeto desenvolvido durante as aulas de Análise e Modelagem de Sistemas. O objetivo é aplicar conceitos de POO (Programação Orientada a Objetos) para gerenciar rotinas escolares, permitindo o cadastro de alunos, criação de tarefas e listagem de atividades.
    Linguagem: Python

---
+++ modelo_incremental_sistema/Repositorio.py
@@ -0,0 +1,13 @@
+import json
+
+def salvar(dados):
+    with open("dados.json","w") as f:
+        json.dump(dados, f)
+
+
+def carregar():
+    try:
+        with open("dados.jason","r") as f:
+            return json.load(f)
+    except:
+        return {}
